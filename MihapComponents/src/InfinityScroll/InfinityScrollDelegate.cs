f2ee240dd2dfef17a0e2f51862d4d90394549c41
dreamer <miqpaa@gmail.com>
2025-04-29 12:17:07 +0300

    remove generic types

--- MihapComponents/src/InfinityScroll/InfinityScrollDelegate.cs
+++ MihapComponents/src/InfinityScroll/InfinityScrollDelegate.cs
@@ -1,3 +1,3 @@
 namespace MihapComponents.InfinityScroll;
 
-public delegate Task<IList<T>> InfinityScrollDelegate<T>(CancellationToken ct);
+public delegate Task InfinityScrollDelegate(CancellationToken ct);
